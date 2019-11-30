da7e79eb1409c94371f62a85852f1f8a32dafd2c
Evgeny Pisemsky <evgeny@pisemsky.com>
2019-11-30 21:10:31 +0300

    deploy

---
+++ precache-manifest.e80c3e378bb65beb224839218d00a4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed19cf08d9e12adad9e",
+    "url": "/css/app.83df5cd9.css"
+  },
+  {
+    "revision": "a180f7a4be1bc1d3ad92b8724c950bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed19cf08d9e12adad9e",
+    "url": "/js/app.7777130b.js"
+  },
+  {
+    "revision": "167a1e099bede1d233c3",
+    "url": "/js/chunk-vendors.e769df2d.js"
+  },
+  {
+    "revision": "10b26111ad41ba42bcdaa8298737c580",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
